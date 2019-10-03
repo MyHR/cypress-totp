d451c64f8c9116e54eb0d9b33741a58593f07ed2
Tom Kühl <hello@tomkuehl.io>
2019-10-03 10:06:23 +0300

    Add ntp client

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const speakeasy = require('speakeasy');
+const { NTPClient } = require('ntpclient');
 
 function sleep(s) {
     return new Promise(resolve => setTimeout(() => resolve(), s * 1000));
 }
+
+const ntp = new NTPClient();
 
 module.exports = async function generateTOTP(args) {
     const threshold = args.threshold || 5;
     if (threshold >= 20) {
         throw new Error('Invalid threshold.');
     }
-    const seconds = new Date().getSeconds();
+    const date = await ntp.getNetworkTime();
+    let dateObj = new Date(date);
+    const seconds = dateObj.getSeconds();
     if (seconds >= 30 - threshold && seconds <= 30) {
         await sleep(31 - seconds);
+        const date = await ntp.getNetworkTime();
+        dateObj = new Date(date);
     }
     if (seconds >= 60 - threshold && seconds <= 60) {
         await sleep(61 - seconds);
+        const date = await ntp.getNetworkTime();
+        dateObj = new Date(date);
     }
     return speakeasy.totp({
         secret: args.secret,
-        encoding: args.encoding || 'base32'
+        encoding: args.encoding || 'base32',
+        time: dateObj.getTime() / 1000
     });
 };
